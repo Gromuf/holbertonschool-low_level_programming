b7ff3db89957cf352c32ec28a7cfa02e5d61eb21
Gromuf <luigi.beaumois@gmail.com>
2024-06-17 12:48:14 +0000

    adding 2nd task and file 1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,6 +4,15 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point of the program
+ *
+ * Description: Generates a random number, computes its last digit,
+ *              and prints whether the last digit is greater than 5, is 0,
+ *              or is less than 6 and not 0.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
