d5bc3fd34ccc83317749b8fa4cd8abb87289fe54
Gromuf <luigi.beaumois@gmail.com>
2024-06-17 12:58:46 +0000

    adding 3rd task and file 2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the lowercase alphabet from 'a' to 'z',
+ *              followed by a newline.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char c;
