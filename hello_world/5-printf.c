a8ffa70547f626ee7961bbe8e4ced1d3fc542390
Gromuf <luigi.beaumois@gmail.com>
2024-06-17 08:54:24 +0000

    adding 6th taskand file 5-printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 /**
+ *main - Entry point
  *
+ * Description: Prints "with proper grammar, but the outcome is a piece of art,"
+ * 		followed by a newline to the standard output.
  *
- *
+ * Return: Always 0 (Succes)
  */
 int main(void)
 {
