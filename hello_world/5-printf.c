248017a4614fc187931f5fb21f3c0d68bf505db3
Gromuf <luigi.beaumois@gmail.com>
2024-06-17 09:05:02 +0000

    adding 6th taskand file 5-printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+
 /**
- *main - Entry point
+ * main - Entry point
  *
- * Description: Prints "with proper grammar, but the outcome is a piece of art,"
- *		followed by a newline to the standard output.
+ * Description:Prints "with proper grammar, but the outcome is a piece of art,"
+ *              followed by a newline to the standard output.
  *
  * Return: Always 0 (Success)
  */
