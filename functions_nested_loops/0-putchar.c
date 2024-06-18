77b1dbd24008d41237e698a59c6f9b47b1b29fea
Gromuf <luigi.beaumois@gmail.com>
2024-06-18 08:51:56 +0000

    adding first task and file 0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,5 +1,17 @@
-#include <stdio.h>
+#include <unistd.h>
 #include "main.h"
+
+/**
+ * _putchar - Prints a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * x - Prints "_putchar" followed by a new line
@@ -9,7 +21,13 @@
  */
 void x(void)
 {
-	printf("_putchar\n");
+	char text[] = "_putchar\n";
+	int i;
+
+	for (i = 0 ; text[i] != '\0' ; i++)
+	{
+		_putchar(text[i]);
+	}
 }
 
 /**
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,6 +2,15 @@
 #define MAIN_H
 
 /**
+ * _putchar - Prints a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
  * x - Prints "_putchar" followed by a new line
  *
  * Description: This function prints the string "_putchar"
