74ad565e7b614e13871de4f5aee2a99c026d4805
Gromuf <luigi.beaumois@gmail.com>
2024-06-18 09:24:49 +0000

    adding 2nd task and file 1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * print_alphabet - Prints the alphabet in lowercase followed by a new line
+ *
+ * Description:
+ * This function prints each character of the alphabet from 'a' to 'z'
+ * using the _putchar function. It ends by printing a newline character.
+ */
 void print_alphabet(void)
 {
 	char i;
