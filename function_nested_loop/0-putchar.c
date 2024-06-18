8bc0c561f4d93105eed95924a079b1072138458d
Gromuf <luigi.beaumois@gmail.com>
2024-06-18 08:04:20 +0000

    adding first task and file 0-putchar.c

---
+++ function_nested_loop/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints "_putchar" followed by a new line.
+ * This is a simple program to demonstrate how to print text to the console.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
