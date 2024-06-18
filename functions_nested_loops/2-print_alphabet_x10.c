331a2240078d9d4a56f0618efb582cf687509810
Gromuf <luigi.beaumois@gmail.com>
2024-06-18 11:45:10 +0000

    adding 3rd task and file 2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,15 @@
 #include "main.h"
 
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times, followed by a newline each time.
+ *
+ * Description:
+ * Uses nested loops to print each letter of the alphabet ('a' to 'z')
+ * 10 times in total, each set followed by a newline character.
+ * Uses the _putchar function to output characters.
+ *
+ * Return: void (no return value)
+ */
 void print_alphabet_x10(void)
 {
 	int i;
