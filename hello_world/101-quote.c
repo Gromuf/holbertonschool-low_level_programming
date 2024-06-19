88d17b52c2cac24725e2d8261617fb2a3927626a
Gromuf <luigi.beaumois@gmail.com>
2024-06-19 12:11:59 +0000

    adding 9th task and file 101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <unistd.h>
 
 /**
- * main - Entry point of the program
- *
- * Description: Prints a specific message to standard error using the write
- * system call and returns 1.
- *
- * Return: Always 1 (indicating an error)
- */
+* main - Prints "and that piece of art is useful\" - Dora Korpar, 2015-10-19",
+* followed by a new line, to standard error.
+*
+* Return: Always 1.
+*/
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
