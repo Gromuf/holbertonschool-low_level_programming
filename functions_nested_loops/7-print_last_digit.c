38e75a6ff6158f931daafde8f955b058c000c37a
Gromuf <luigi.beaumois@gmail.com>
2024-06-19 08:11:55 +0000

    adding 8th task and file 7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @i: The number to be checked
+ *
+ * Description: This function returns the last digit of an integer.
+ * If the last digit is negative, it converts it to positive before returning.
+ *
+ * Return: The last digit of the number
+ */
 int print_last_digit(int i)
 {
-	return (i % 10);
+	int lastDigit = i % 10;
+
+	if (lastDigit < 0)
+	{
+		lastDigit = -lastDigit;
+	}
+
+	_putchar('0' + lastDigit);
+	return (lastDigit);
 }
