02bb5b4f14f59d4a90856c39e2d95d959132c3e9
Gromuf <luigi.beaumois@gmail.com>
2024-06-19 08:45:27 +0000

    adding 10th task and file 9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,16 +9,20 @@ void times_table(void)
 	{
 		for (j = 0 ; j < 10 ; j++)
 		{
+			if (i * j < 10)
+			{
+				_putchar('0' + i * j);
+			}
+			else
+			{
+				-putchar('0' + i * j / 10);
+				_putchar('0' + i * j % 10);
+			}
 			if (j < 9)
 			{
+				_putchar(',');
 				_putchar(' ');
-				if (i * j <= 9)
-				{
-					_putchar(' ');
-				}
 			}
-			_putchar('0' + i * j);
-			_putchar(',');
 		}
 		_putchar('\n');
 	}
