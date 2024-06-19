5a15c056b8bd1356ff4cf7a32179f5976e12adec
Gromuf <luigi.beaumois@gmail.com>
2024-06-19 07:53:59 +0000

    adding 6th task and file 5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,6 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * print_sign - Determines and prints the sign of a number
+ * @i: The number to be checked
+ *
+ * Description: This function prints '+' if the number is positive,
+ * '0' if the number is zero, and '-' if the number is negative.
+ *
+ * Return: 1 if the number is positive, 0 if the number is zero,
+ * -1 if the number is negative
+ */
 int print_sign(int i)
 {
 	if (i > 0)
