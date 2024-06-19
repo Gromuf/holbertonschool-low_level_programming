c58bce86c48e19870ae62d43f4945b57c7977599
Gromuf <luigi.beaumois@gmail.com>
2024-06-19 09:34:42 +0000

    adding 11th task and file 10-add.c

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,5 +1,15 @@
 #include "main.h"
 
+/**
+ * add - Adds two integers
+ * @x: The first integer to be added
+ * @y: The second integer to be added
+ *
+ * Description: This function takes two integers as arguments and returns
+ * their sum.
+ *
+ * Return: The sum of the two integers
+ */
 int add(int x, int y)
 {
 	return (x + y);
