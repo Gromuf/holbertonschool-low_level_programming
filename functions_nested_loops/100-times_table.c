5b10184df2a615eddeac4350db68e678e4137717
Gromuf <luigi.beaumois@gmail.com>
2024-06-20 12:07:00 +0000

    adding 13th task and file 100-times_table.c

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -2,6 +2,9 @@
 
 void print_times_table(int n)
 {
+	if (n > 15 || n < 0)
+		return;
+
 	int i;
 	int j;
 
@@ -9,38 +12,33 @@ void print_times_table(int n)
 	{
 		for (j = 0 ; j <= n ; j++)
 		{
-			if (i * j == 0 && j > 0)
-			{
-				_putchar(' ');
-				/*_putchar(' ');*/
-			}
-			if (i * j == 0 && j < n)
-			{
-				_putchar('0');
+			  if (j != 0)
+			  {
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-			}
-			if (i * j == 0 && j == n)
-			{
-				_putchar('0');
-			}
-			else if (i * j <= n && i * j > 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
+			  }
+
+			  if (i * j < 10)
+			  {
+				if (j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
 				_putchar('0' + i * j);
-			}
-			else if (i * j >= 10)
-			{
+			  }
+			  else if (i * j < 100)
+			  {
+				_putchar(' ');
 				_putchar('0' + i * j / 10);
 				_putchar('0' + i * j % 10);
-			}
-			if (j < n && i * j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			  }
+			  else
+			  {
+				_putchar('0' + i * j / 100);
+				_putchar('0' + (i * j / 10) % 10);
+				_putchar('0' + i * j % 10);
+			  }
 		}
 		_putchar('\n');
 	}
