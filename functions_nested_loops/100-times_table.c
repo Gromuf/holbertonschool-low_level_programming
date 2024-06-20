4c766ce02f3b525a1f66945a49ca97ed616988b4
Gromuf <luigi.beaumois@gmail.com>
2024-06-20 11:44:12 +0000

    adding 13th task and file 100-times_table.c

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -13,13 +13,13 @@ void print_times_table(int n)
 			{
 				_putchar(' ');
 			}
-			if (i * j == 0 && j < 9)
+			if (i * j == 0 && j < n)
 			{
 				_putchar('0');
 				_putchar(',');
 				_putchar(' ');
 			}
-			if (i * j == 0 && j == 9)
+			if (i * j == 0 && j == n)
 			{
 				_putchar('0');
 			}
@@ -33,7 +33,7 @@ void print_times_table(int n)
 				_putchar('0' + i * j / 10);
 				_putchar('0' + i * j % 10);
 			}
-			if (j < 9 && i * j != 0)
+			if (j < n && i * j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
