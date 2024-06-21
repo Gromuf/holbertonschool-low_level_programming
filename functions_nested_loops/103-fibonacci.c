4e13b5fc04904c59ab1bba2ea34527d51472fee1
Gromuf <luigi.beaumois@gmail.com>
2024-06-21 08:09:55 +0000

    adding 16th task nd file 103-fibonacci.c

--- functions_nested_loops/103-fibonacci.c
+++ functions_nested_loops/103-fibonacci.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: Computes the sum of the even-valued terms in the Fibonacci
+ * sequence whose values do not exceed 4,000,000 and prints the result.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	long a = 1;
