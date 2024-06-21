666d908124a76cedae23031b2eb0e0ab9a49989b
Gromuf <luigi.beaumois@gmail.com>
2024-06-21 08:28:20 +0000

    adding 17th taskand file 104-fibonacci.c

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -2,18 +2,33 @@
 
 int main(void)
 {
-	unsigned long a = 1;
-	unsigned long b = 2;
-	unsigned long result;
+	unsigned long a1 = 1, a2 = 0;
+	unsigned long b1 = 2, b2 = 0;
+	unsigned long temp1, temp2;
+	unsigned long max = 1000000000;
 	int i;
 
-	printf("%lu, %lu", a, b);
+	printf("%lu, %lu", a1, b1);
 	for (i = 3 ; i <= 98 ; i++)
 	{
-		result = a + b;
-		printf(", %lu", result);
-		a = b;
-		b = result;
+		temp1 = a1 + b1;
+		temp2 = a2 + b2;
+
+		if (temp2 >= max)
+		{
+			temp1 += temp2 / max;
+			temp2 = temp2 % max;
+		}
+
+		printf(", %lu", temp1);
+		if (temp2 > 0)
+		{
+			printf("%09lu", temp2);
+		}
+		a1 = b1;
+		a2 = b2;
+		b1 = temp1;
+		b2 = temp2;
 	}
 	printf("\n");
 	return (0);
