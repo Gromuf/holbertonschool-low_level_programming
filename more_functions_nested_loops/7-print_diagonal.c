5201055f89b8f82fa5c73cff672bf00a45334990
Gromuf <luigi.beaumois@gmail.com>
2024-06-24 08:33:59 +0000

    adding 8th task and file 7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		for (j = 0 ; j < i ; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
