885d56e3dc4d79b5e924024acc1adff3f5f6fdb5
Gromuf <luigi.beaumois@gmail.com>
2024-06-24 12:42:12 +0000

    adding 6th task and file 5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -4,14 +4,17 @@ void rev_string(char *s)
 {
 	int length = 0;
 	int i;
+	char temp;
 
 	while (s[length] != '\0')
 	{
 		length++;
 	}
-	for (i = length - 1 ; i >= 0 ; i--)
+	for (i = 0 ; i < length / 2 ; i++)
 	{
-		_putchar(s[i]);
+		temp = s[i];
+		s[i] = s[length - 1 - i];
+		s[length - 1 - i] = temp;
 	}
 	_putchar('\n');
 }
