cffc72c47c4f10b3ad8419045c4c411f2dda4213
Gromuf <luigi.beaumois@gmail.com>
2024-06-24 09:15:07 +0000

    adding 11th task and file 10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.c"
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1 ; i <= size ; i++)
+		{
+			for (j = 0 ; j < size - i ; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = size - i ; j < size ; j++)
+			{
+				_putchar('#');
+			}
+		}
+		_putchar('\n');
+	}
+}
