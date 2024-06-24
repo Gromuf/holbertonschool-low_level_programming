3bf30c7d772e2ba9e72d6dfe41f851506500d83a
Gromuf <luigi.beaumois@gmail.com>
2024-06-24 11:51:08 +0000

    adding 2nd task and file 1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+void swap_int(int *a, int *b)
+{
+	int x = *a;
+	*a = *b;
+	*b = x;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -9,4 +9,6 @@ static inline int _putchar(char c)
 
 void reset_to_98(int *n);
 
+void swap_int(int *a, int *b);
+
 #endif /*NAIN_H*/
