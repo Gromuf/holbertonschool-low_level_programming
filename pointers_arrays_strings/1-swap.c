617cf17f6a92e73f9dca8f91a940abdd63b7df02
Gromuf <luigi.beaumois@gmail.com>
2024-06-24 11:52:08 +0000

    adding 2nd task and file 1-swap.c

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+* swap_int - Swaps the integer values pointed to by two pointers.
+* @a: Pointer to the first integer.
+* @b: Pointer to the second integer.
+*
+* This function swaps the integer values stored at the memory locations
+* pointed to by @a and @b.
+*/
 void swap_int(int *a, int *b)
 {
 	int x = *a;
