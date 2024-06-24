b00f8d0e7238be6e62eb9377442a4bd0d05369a6
Gromuf <luigi.beaumois@gmail.com>
2024-06-24 12:02:06 +0000

    adding 3rd task and file 2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,15 @@
 #include "main.h"
 
+/**
+* _strlen - Calculates the length of a null-terminated string.
+* @str: Pointer to the null-terminated string.
+*
+* Description:
+* This function calculates and returns the length of the string pointed to by @str,
+* excluding the null terminator '\0'.
+*
+* Return: The length of the string pointed to by @str.
+*/
 int _strlen(char *s)
 {
 	int result = 0;
