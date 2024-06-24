bb3faa18c8c749459eef048eaca242e0f00a976a
Gromuf <luigi.beaumois@gmail.com>
2024-06-24 12:06:01 +0000

    adding 3rd task and file 2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -5,7 +5,8 @@
 * @s: Pointer to the string.
 *
 * Description:
-* This function calculates and returns the length of the string pointed to by @s,
+* This function calculates and returns
+* the length of the string pointed to by @s,
 * excluding the null terminator '\0'.
 *
 * Return: The length of the string pointed to by @s.
