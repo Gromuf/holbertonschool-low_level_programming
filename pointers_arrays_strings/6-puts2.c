71212f9539d162893308858103a0c7df2cd16805
Gromuf <luigi.beaumois@gmail.com>
2024-06-24 12:55:43 +0000

    adding 7th task and file 6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+* puts2 - Prints every other character of a string,
+* starting with the first character.
+* @str: Pointer to the null-terminated string to be printed.
+*
+* Description:
+* This function prints every other character of the string pointed to by @str,
+* starting with the first character, followed by a newline character '\n'.
+*/
 void puts2(char *str)
 {
 	int i = 0;
