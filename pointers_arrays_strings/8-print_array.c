dedb601f0b6ff77e23ba17e13c4a8a36f225cef2
Gromuf <luigi.beaumois@gmail.com>
2024-06-24 13:45:25 +0000

    adding 9th task and file 8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+* print_array - Prints n elements of an array of integers.
+* @a: Pointer to the array of integers.
+* @n: Number of elements to print.
+*
+* Description:
+* This function prints n elements of the array pointed to by @a.
+* Each number is followed by a comma and a space, except the last number,
+* which is followed by a newline.
+*/
 void print_array(int *a, int n)
 {
 	int i;
