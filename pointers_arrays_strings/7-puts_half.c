54c440c0530c0912906b62281144274105167e6e
Gromuf <luigi.beaumois@gmail.com>
2024-06-24 13:06:01 +0000

    adding 8th task and file 7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+void puts_half(char *str)
+{
+	int length = 0;
+	int midLength;
+	int i;
+
+	while (str[legnth] != 0)
+	{
+		length++;
+	}
+	if (length % 2 == 0)
+		midLength = length / 2;
+	else
+		midLength = (length - 1) / 2;
+	for (i = midLength ; i < length ; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
