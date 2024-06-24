32b1bbdb38429c2dcad5ad1b023b06ee3dfd5531
Gromuf <luigi.beaumois@gmail.com>
2024-06-24 12:11:37 +0000

    adding 4th task and file 3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+* _puts - Outputs a string followed by a newline character.
+* @str: Pointer to the null-terminated string to be output.
+*
+* Description:
+* This function outputs the string pointed to by @str to the standard output,
+* character by character, using the _putchar function. It appends a newline
+* character '\n' after printing the entire string.
+*/
 void _puts(char *str)
 {
 	while (*str != 0)
