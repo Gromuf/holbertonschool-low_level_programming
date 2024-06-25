5cee5526a7d297fe62fb7bd903648b760c5f4035
Gromuf <luigi.beaumois@gmail.com>
2024-06-25 12:01:38 +0000

    adding 8th task and file 7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -3,40 +3,23 @@
 char *leet(char *str)
 {
 	char *x = str;
+	char map[256] = {0};
+
+	map['a'] = '4';
+	map['A'] = '4';
+	map['e'] = '3';
+	map['E'] = '3';
+	map['o'] = '0';
+	map['O'] = '0';
+	map['t'] = '7';
+	map['T'] = '7';
+	map['l'] = '1';
+	map['L'] = '1';
 
 	while (*x != '\0')
 	{
-		int i = 0;
-
-		while (i < 5)
-		{
-			if (*x == 'a' || *x == 'A')
-			{
-				*x = '4';
-				break;
-			}
-			else if (*x == 'e' || *x == 'E')
-			{
-				*x = '3';
-				break;
-			}
-			else if (*x == 'o' || *x == 'O')
-			{
-				*x = '0';
-				break;
-			}
-			else if (*x == 't' || *x == 'T')
-			{
-				*x = '7';
-				break;
-			}
-			else if (*x == 'l' || *x == 'L')
-			{
-				*x = '1';
-				break;
-			}
-			i++;
-		}
+		if (map[*x] != 0)
+			*x = map[*x];
 		x++;
 	}
 	return (str);
