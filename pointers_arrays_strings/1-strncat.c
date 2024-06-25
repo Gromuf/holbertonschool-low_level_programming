7368b0200860040b1ebbee681afcbf865216ba5c
Gromuf <luigi.beaumois@gmail.com>
2024-06-25 08:40:03 +0000

    adding 2nd task and file 1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,15 @@
 #include "main.h"
 
+/**
+* _strncat - Concatenates at most n bytes of the src string to the dest string,
+*            overwriting the terminating null byte (\0) at the end of dest,
+*            and then adds a terminating null byte.
+* @dest: Pointer to the destination buffer which should contain a string.
+* @src: Pointer to the source string to be appended to dest.
+* @n: Maximum number of bytes to be used from src.
+*
+* Return: Pointer to the resulting string dest.
+*/
 char *_strncat(char *dest, char *src, int n)
 {
 	char *start = dest;
