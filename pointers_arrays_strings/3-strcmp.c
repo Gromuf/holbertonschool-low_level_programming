5b84c41cffd4cad3ecc05ba1f116e43fa34ad855
Gromuf <luigi.beaumois@gmail.com>
2024-06-25 08:57:26 +0000

    adding 4th task and file 3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,15 @@
 #include "main.h"
 
+
+/**
+* _strcmp - Compares two strings.
+* @s1: The first string to be compared.
+* @s2: The second string to be compared.
+*
+* Return: An integer less than, equal to, or greater than zero if s1 (or the
+*         first n bytes thereof) is found, respectively, to be less than,
+*         to match, or be greater than s2.
+*/
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 && (*s1 == *s2))
