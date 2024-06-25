de405bede6c676b22feb9058fda909804d3350e6
Gromuf <luigi.beaumois@gmail.com>
2024-06-25 08:34:01 +0000

    adding first task and file 0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+* _strcat - Concatenates the src string to the dest string,
+*           overwriting the terminating null byte (\0) at the end of dest,
+*           and then adds a terminating null byte.
+* @dest: Pointer to the destination buffer which should contain a string.
+* @src: Pointer to the source string to be appended to dest.
+*
+* Return: Pointer to the resulting string dest.
+*/
 char *_strcat(char *dest, char *src)
 {
 	char *start = dest;
