f977321ed8ea39014a15b5ae26ee96f4614e9410
Gromuf <luigi.beaumois@gmail.com>
2024-06-25 07:43:44 +0000

    adding 10th task and file 9-strcpy.c

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+* _strcpy - Copies the string pointed to by src, including the
+*           terminating null byte (\0), to the buffer pointed to by dest.
+* @dest: Pointer to the destination buffer where the content is to be copied.
+* @src: Pointer to the source string to be copied.
+*
+* Return: Pointer to the destination string (dest).
+*/
 char *_strcpy(char *dest, char *src)
 {
 	char *start = dest;
