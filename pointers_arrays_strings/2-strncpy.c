1c4d9f5f3a09501bb5be8675eeb292f14920cd1e
Gromuf <luigi.beaumois@gmail.com>
2024-06-25 08:49:09 +0000

    adding 3rd task and file 3-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,17 @@
 #include "main.h"
 
+/**
+* _strncpy - Copies up to n characters from the src string to the dest string.
+*            If the length of src is less than n, the remainder of dest will
+*            be padded with null bytes (\0). If the length of src is greater
+*            than or equal to n, the string pointed to by dest will not be
+*            null-terminated.
+* @dest: Pointer to the destination buffer.
+* @src: Pointer to the source string.
+* @n: Maximum number of characters to be copied from src.
+*
+* Return: Pointer to the destination string dest.
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
