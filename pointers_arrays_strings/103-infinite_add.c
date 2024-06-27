76f91a539542a285831645ef681a69d62c5197c6
Gromuf <luigi.beaumois@gmail.com>
2024-06-27 18:30:32 +0000

    adding 12th task and file 103-infinite_add.c

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -2,6 +2,15 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+* infinite_add - Adds two numbers represented as strings.
+* @n1: The first number as a string.
+* @n2: The second number as a string.
+* @r: The buffer to store the result.
+* @size_r: The size of the buffer.
+* 
+* Return: A pointer to the result, or 0 if the result cannot be stored in r.
+*/
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int length1 = strlen(n1);
