85f43b795786f503dfe439cff87266781ea226c8
Gromuf <luigi.beaumois@gmail.com>
2024-06-27 18:29:10 +0000

    adding 12th task and file 103-infinite_add.c

---
+++ pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int length1 = strlen(n1);
+	int length2 = strlen(n2);
+	int carry = 0;
+	int i = length1 - 1;
+	int j = length2 - 1;
+	int k = 0;
+	int max_length = length1 > length2 ? length1 : length2;
+
+	if (size_r <= max_length + 1)
+		return (0);
+	while (i >= 0 || j >= 0 || carry)
+	{
+		int sum = carry;
+
+		if (i >= 0)
+		{
+			sum += n1[i] - '0';
+			i--;
+		}
+		if (j >= 0)
+		{
+			sum += n2[j] - '0';
+			j--;
+		}
+		if (k >= size_r - 1)
+			return (0);
+		r[k] = (sum % 10) + '0';
+		carry = sum / 10;
+		k++;
+	}
+	r[k] = '\0';
+	for (i = 0, j = k - 1 ; i < j ; i++, j--)
+	{
+		char temp = r[i];
+
+		r[i] = r[j];
+		r[j] = temp;
+	}
+	return (r);
+}
