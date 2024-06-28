ab00c3543045b5477e9ca8b8fb2a61eeac1dc624
Gromuf <luigi.beaumois@gmail.com>
2024-06-28 07:53:53 +0000

    adding 13th task and file 104-print_buffer.c

--- pointers_arrays_strings/104-print_buffer.c
+++ pointers_arrays_strings/104-print_buffer.c
@@ -22,9 +22,10 @@ void print_buffer(char *b, int size)
 				printf("%02x", (unsigned char)b[i + j]);
 			else
 				printf("  ");
-			if (j % 2 == 1)
+			if (j % 2 == 1 && j != 9)
 				printf(" ");
 		}
+		printf(" ");
 	for (j = 0 ; j < 10 ; j++)
 	{
 		if (i + j < size)
@@ -37,7 +38,7 @@ void print_buffer(char *b, int size)
 				printf(".");
 		}
 		else
-			printf(" ");
+			break;
 	}
 	printf("\n");
 	}
