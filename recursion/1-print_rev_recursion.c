6047374887b605885ac6d78bd88f080a4f0647da
Gromuf <luigi.beaumois@gmail.com>
2024-07-01 13:02:01 +0000

    adding 2nd task and file 1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: Pointer to the string to be printed
+ *
+ * This function prints the string @s in reverse recursively,
+ * character by character, using the _putchar function.
+ */
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
