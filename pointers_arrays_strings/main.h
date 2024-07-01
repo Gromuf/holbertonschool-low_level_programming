df64c38d3822ce77d4bab7bf4c5475bfa074dece
Gromuf <luigi.beaumois@gmail.com>
2024-07-01 10:46:28 +0000

    adding 8th task and file 7-print_chessboard

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,11 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <unistd.h>
 
-static inline int _putchar(char c)
-{
-	return write(1, &c, 1);
-}
+int _putchar(char c);
 
 void reset_to_98(int *n);
 
@@ -64,4 +60,6 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
+
+void print_chessboard(char (*a)[8]);
 #endif /*NAIN_H*/
