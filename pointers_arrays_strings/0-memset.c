50569929504edc0d97076f980f5d691edd25bc1b
Gromuf <luigi.beaumois@gmail.com>
2024-07-01 08:18:01 +0000

    adding first task and file 0-memset.c

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,9 +1,19 @@
 #include "main.h"
 
+/**
+ * _memset - Fills memory with a constant byte.
+ * @s: The pointer to the memory area to be filled.
+ * @b: The byte to fill the memory area with.
+ * @n: The number of bytes to be filled.
+ *
+ * Return: A pointer to the memory area `s`.
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
+	if (n <= 0)
+		return (s);
 	i = 0;
 	while (i < n)
 	{
