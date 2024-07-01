a751b27ad5ff8920f1b5abe5da77112652cdfa63
Gromuf <luigi.beaumois@gmail.com>
2024-07-01 08:41:48 +0000

    adding 3rd task and file 2-strchr.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -56,4 +56,6 @@ void print_buffer(char *b, int size);
 char *_memset(char *s, char b, unsigned int n);
 
 char *_memcpy(char *dest, char *src, unsigned int n);
+
+char *_strchr(char *s, char c);
 #endif /*NAIN_H*/
