0023e29da20f8de23519656837cdbb2e5f12fdc5
Gromuf <luigi.beaumois@gmail.com>
2024-07-01 09:39:35 +0000

    adding 5th task and file 4-strpbrk.c

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * _strpbrk - Locate the first occurrence in the string s of any
+ *            character in the string accept
+ * @s: The string to search
+ * @accept: The string containing the characters to match
+ *
+ * Return: Pointer to the first occurrence in s of any character in accept,
+ *         or NULL if no such character is found
+ */
 char *_strpbrk(char *s, char *accept)
 {
 	char *i;
