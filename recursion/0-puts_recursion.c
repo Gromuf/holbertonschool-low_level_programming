e736859e7ff34cf5355c1a8a6e69f9c327d11ed4
Gromuf <luigi.beaumois@gmail.com>
2024-07-01 12:23:57 +0000

    adding 1st task and file 0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
+/**
+ * _puts_recursion - Prints a string recursively followed by a new line
+ * @s: Pointer to the string to be printed
+ *
+ * This function prints the string @s recursively, character by character,
+ * using the _putchar function, until it reaches the null terminator '\0'.
+ * After printing the entire string, it prints a new line character '\n'.
+ */
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
 		_putchar(*s);
-		s++;
-		_puts_recursion(s);
+		_puts_recursion(s + 1);
 	}
 	else
 		_putchar('\n');
