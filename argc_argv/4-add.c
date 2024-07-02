ea7b003c2a150134b29b8227b96e16963f4f219b
Gromuf <luigi.beaumois@gmail.com>
2024-07-02 09:48:35 +0000

    adding the rest of mandatory task

---
+++ argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int result = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1 ; i < argc ; i++)
+	{
+		char *endptr;
+		long num = strtol(argv[i], &endptr, 10);
+		
+		if (*endptr != '\0' || num < 0 || num > INT_MAX)
+		{
+			printf("ERROR\n");
+			return (1);
+		}
+		result += (int)num;
+	}
+	printf("%d\n", result);
+	return (0);
+}
