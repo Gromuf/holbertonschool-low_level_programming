21249ef832bf1bcd03793f082bdbdc17960bbaed
Gromuf <luigi.beaumois@gmail.com>
2024-07-03 10:34:32 +0000

    correcting

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -17,6 +17,13 @@ int palindrome_rec(char *s, int start, int end)
 		return (0);
 	return (palindrome_rec(s, start + 1, end - 1));
 }
+
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: The string to check.
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise.
+ */
 int is_palindrome(char *s)
 {
 	int length = strlen(s);
