904b7d5eb596503401f535853526f1e04a5219bf
Gromuf <luigi.beaumois@gmail.com>
2024-07-03 08:40:25 +0000

    adding 9th task and file 100-set_string.c

--- pointers_arrays_strings/100-set_string.c
+++ pointers_arrays_strings/100-set_string.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * set_string - Sets the value of a pointer-to-pointer to a new string.
+ * @s: Pointer to a pointer that will be updated.
+ * @to: Pointer to the new string value.
+ *
+ * This function updates the pointer @s to point to the string @to.
+ */
 void set_string(char **s, char *to)
 {
 	*s = to;
