8fca6950a51c22aa2969d082e7cc0432effc14be
Gromuf <luigi.beaumois@gmail.com>
2024-07-03 10:53:25 +0000

    correction

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Multiplies two numbers provided as command line arguments.
+ * @argc: The number of command line arguments.
+ * @argv: An array of strings representing the command line arguments.
+ *
+ * Return: 0 on success, 1 if the number of arguments is incorrect.
+ */
 int main(int argc, char *argv[])
 {
 	if (argc != 3)
