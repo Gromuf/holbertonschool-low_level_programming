5f2672ed86f944a0578736701403a42eada3b41d
Gromuf <luigi.beaumois@gmail.com>
2024-07-10 09:42:11 +0000

    correcting

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -2,46 +2,6 @@
 #include <stdlib.h>
 
 /**
-* _strlen - Calculates the length of a string.
-* @s: Pointer to the string.
-*
-* Return: The length of the string pointed to by @s.
-*/
-int _strlen(char *s)
-{
-	int result = 0;
-
-	while (*s != '\0')
-	{
-		result++;
-		s++;
-	}
-	return (result);
-}
-
-/**
-* _strcpy - Copies the string pointed to by src, including the
-*           terminating null byte (\0), to the buffer pointed to by dest.
-* @dest: Pointer to the destination buffer where the content is to be copied.
-* @src: Pointer to the source string to be copied.
-*
-* Return: Pointer to the destination string (dest).
-*/
-char *_strcpy(char *dest, char *src)
-{
-	char *start = dest;
-
-	while (*src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-	*dest = '\0';
-	return (start);
-}
-
-/**
 * _strdup - Duplicates a string in memory.
 * @str: The string to duplicate.
 *
@@ -50,14 +10,23 @@ char *_strcpy(char *dest, char *src)
 */
 char *_strdup(char *str)
 {
-	int length = _strlen(str);
 	char *res;
+	int i;
+	int length = 0;
 
 	if (str == NULL)
 		return (NULL);
+	for (i = 0 ; str[i] ; i++)
+	{
+		length++;
+	}
 	*res = malloc(sizeof(char) * (length + 1));
 	if (res == NULL)
 		return (NULL);
-	_strcpy(res, str);
+	for (i = 0 ; str[i] ; i++)
+	{
+		res[i] = str[i];
+	}
+	res[length] = '\0';
 	return (res);
 }
