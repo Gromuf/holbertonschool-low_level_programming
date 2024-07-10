ee1a527cb20ebd191ca62e7d1910b5ae6bd3ba85
Gromuf <luigi.beaumois@gmail.com>
2024-07-10 11:45:38 +0000

    good ?

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -7,7 +7,8 @@
  * @s2: The second string.
  * @n: The maximum number of bytes to include from s2.
  *
- * Return: A pointer to the newly allocated space in memory containing the concatenated string.
+ * Return: A pointer to the newly allocated space in memory
+ * containing the concatenated string.
  *         If malloc fails, return NULL.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
@@ -19,32 +20,26 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int length2 = 0;
 	unsigned int length;
 
-	while (s1[length1] != '\0')
+	if (s1 != NULL)
 	{
-		length1++;
+		while (s1[length1] != '\0')
+			length1++;
 	}
-	while (s2[length2] != '\0')
+	if (s2 != NULL)
 	{
-		length2++;
+		while (s2[length2] != '\0')
+			length2++;
 	}
 	if (n > length2)
-	{
 		n = length2;
-	}
 	length = length1 + n;
 	res = malloc(sizeof(char) * (length + 1));
 	if (res == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0 ; i < length1 ; i++)
-	{
 		res[i] = s1[i];
-	}
 	for (j = 0 ; j < n ; j++)
-	{
 		res[i + j] = s2[j];
-	}
 	res[i + j] = '\0';
 	return (res);
 }
