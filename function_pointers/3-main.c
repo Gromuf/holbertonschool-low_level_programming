2af547d4e0e0e068c74bc2ce21a9ae9fe3e8bafa
Gromuf <luigi.beaumois@gmail.com>
2024-07-16 08:57:28 +0000

    last task

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -9,32 +9,32 @@
  */
 int main(int argc, char *argv[])
 {
-    int num1;
-    int num2;
-    char *op;
+	int num1;
+	int num2;
+	char *op;
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
-    op = argv[2];
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
 
-    if ((*op == '%' || *op == '/') && num2 == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
+	if ((*op == '%' || *op == '/') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-    if (get_op_func(op) == NULL || op[1] != '\0')
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    printf("%d\n", get_op_func(op)(num1, num2));
-    return (0);
+	printf("%d\n", get_op_func(op)(num1, num2));
+	return (0);
 }
