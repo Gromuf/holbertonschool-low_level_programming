2c2925cd4069cb12c153f93dd3eb55313f634ac6
Beaumois <louis.beaumois@etudiant.univrennes1.fr>
2024-07-17 10:39:08 +0200

    adding betty

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -2,24 +2,27 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * print_numbers - Prints numbers followed by a separator.
+ * @separator: The string to be printed between numbers.
+ * @n: The number of integers passed to the function.
+ *
+ * This function takes a variable number of integers and prints them.
+ * If a separator is provided, it is printed between the numbers.
+ * If the separator is NULL, the numbers are printed without any separator.
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list nums;
 	unsigned int i;
 
 	va_start(nums, n);
-	if (*separator == NULL)
+
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			printf("%d", va_arg(nums, int));
-		}
+		printf("%d", va_arg(nums, int));
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
 	}
-	else
-	{
-		for (i = 0; i < n; i++)
-		{
-			printf("%d%c", va_arg(nums, int), separator);
-		}
-	}
+	printf("\n");
 }
