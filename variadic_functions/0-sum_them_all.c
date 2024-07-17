000bed5d8b2a647093e91e016b1cdd2bdd7aac81
Beaumois <louis.beaumois@etudiant.univrennes1.fr>
2024-07-17 10:15:55 +0200

    test

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,13 +12,17 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int res = 0;
 
+	if (n == 0)
+		return (0);
+
 	va_start(nums, n);
 
 	for (i = 0; i < n; i++)
 	{
 		res += va_arg(nums, int);
 	}
-	va_end(nums);
-	return (res);
 
+	va_end(nums);
+
+	return (res);
 }
