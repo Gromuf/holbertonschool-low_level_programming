5aaebdba01d139475339229f8f24428719733e9e
Gromuf <luigi.beaumois@gmail.com>
2024-07-29 07:59:54 +0000

    adding tast task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -12,10 +12,10 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("[%d] %s", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
 		result++;
-		h->next;
+		h = h->next;
 	}
 	return (result);
 }
