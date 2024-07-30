27b7e3f008f8857373110774629385de6483593b
Beaumois <louis.beaumois@etudiant.univrennes1.fr>
2024-07-30 10:12:33 +0200

    task 3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -2,26 +2,30 @@
 #include <string.h>
 
 /**
- * add_node - Adds a new node at the beginning of a list_t list
+ * add_node_end - Adds a new node at the end of a list_t list
  * @head: Double pointer to the list_t list
  * @str: String to be duplicated and added to the new node
  *
  * Return: Address of the new element, or NULL if it failed
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *newNode;
 	char *dup_str;
 	int len = 0;
+	list_t *tempNode;
 
 	if (str == NULL)
 		return (NULL);
 	dup_str = strdup(str);
+
 	if (dup_str == NULL)
 		return (NULL);
 	while (str[len])
 		len++;
+
 	newNode = malloc(sizeof(list_t));
+
 	if (newNode == NULL)
 	{
 		free(dup_str);
@@ -29,7 +33,19 @@ list_t *add_node(list_t **head, const char *str)
 	}
 	newNode->str = dup_str;
 	newNode->len = len;
-	newNode->next = *head;
-	*head = newNode;
+	newNode->next = NULL;
+
+	if (*head == NULL)
+		*head = newNode;
+
+	else
+	{
+		tempNode = *head;
+
+		while (tempNode != NULL)
+			tempNode = tempNode->next;
+
+		tempNode->next = newNode;
+	}
 	return (newNode);
 }
