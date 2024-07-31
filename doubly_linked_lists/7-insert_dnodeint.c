f2cb3d1a58968c1811fda8ce3e090040c1a8cde8
Beaumois <louis.beaumois@etudiant.univrennes1.fr>
2024-07-31 14:45:13 +0200

    task 7

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *newNode;
+	dlistint_t *temp;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	while (idx != 1)
+	{
+		temp = temp->next;
+		if (temp == NULL)
+			return (NULL);
+	}
+
+	if (temp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	newNode = malloc(sizeof(dlistint_t));
+	if (newNode == NULL)
+		return (NULL);
+		
+	newNode->n = n;
+	newNode->prev = temp;
+	newNode->next = temp->next;
+	temp->next->prev = newNode;
+	temp->next = newNode;
+
+	return (newNode);
+}
